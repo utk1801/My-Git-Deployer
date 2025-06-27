582e425e38e69b2193f6128b245e213635f45f47
Utkarsh <admin@utk1801.github.io>
2025-06-26 19:36:04 -0700

    integrate AWS S3 with Docker

---
+++ api-server/index.js
@@ -0,0 +1,116 @@
+const express = require("express");
+const { generateSlug } = require("random-word-slugs");
+const { Server } = require("socket.io");
+// const Redis = require("ioredis");
+const { spawn } = require("child_process");
+require("dotenv").config();
+
+const app = express();
+const PORT = 9000;
+let projectSlug = "undefined";
+// const subscriber = new Redis("");
+
+// const io = new Server({ cors: '*' })
+
+// io.on('connection', socket => {
+//     socket.on('subscribe', channel => {
+//         socket.join(channel)
+//         socket.emit('message', `Joined ${channel}`)
+//     })
+// })
+
+// io.listen(9002, () => console.log('Socket Server 9002'))
+
+app.use(express.json());
+
+app.post("/project", async (req, res) => {
+  const { gitUrl, slug } = req.body;
+  projectSlug = slug ? slug : generateSlug();
+  console.log(`Using project slug as ${projectSlug}`);
+
+  const accessKeyId = process.env.ACCESS_KEY_ID;
+  const secretAccessKey = process.env.SECRET_ACCESS_KEY;
+  const dockerImageName = process.env.DOCKER_IMAGE;
+
+  console.log(`${accessKeyId}_${secretAccessKey}_${dockerImageName}`);
+
+  await runDockerImageAsync(
+    dockerImageName,
+    gitUrl,
+    projectSlug,
+    accessKeyId,
+    secretAccessKey
+  );
+
+  return res.json({
+    status: "queued",
+    data: { projectSlug, url: `http://${projectSlug}.localhost:8000` },
+  });
+});
+
+async function runDockerImageAsync(
+  dockerImage,
+  gitUrl,
+  projectSlug,
+  accessKeyId,
+  secretAccessKey
+) {
+  console.log(`Docker image name : ${dockerImage}`);
+
+  const containerName = projectSlug; //set docker container name as generatedSlug.
+  //Command to run a container and automatically remove it on exit.
+  const dockerArgs = [
+    "run",
+    "--rm", //This flag ensures automatic container removal upon exit.
+    "--name",
+    containerName,
+    "-e",
+    `GIT_REPOSITORY__URL=${gitUrl}`,
+    "-e",
+    `PROJECT_ID=${projectSlug}`,
+    "-e",
+    `ACCESS_KEY_ID=${accessKeyId}`,
+    "-e",
+    `SECRET_ACCESS_KEY=${secretAccessKey}`,
+    dockerImage,
+  ];
+
+  console.log(`Attempting to run Docker image: ${dockerImage}`);
+
+  const dockerProcess = spawn("docker", dockerArgs);
+  console.log(`Docker args: ${dockerArgs}`);
+
+  dockerProcess.stdout.on("data", (data) => {
+    console.log(`Docker stdout: ${data}`);
+  });
+
+  dockerProcess.stderr.on("data", (data) => {
+    console.error(`Docker stderr: ${data}`);
+  });
+
+  dockerProcess.on("close", (code) => {
+    if (code === 0) {
+      console.log(`Docker container "${dockerImage}" started successfully.`);
+    } else {
+      console.error(
+        `Docker process exited with code ${code}. Failed to start container.`
+      );
+    }
+  });
+
+  dockerProcess.on("error", (err) => {
+    console.error(`Failed to start Docker process: ${err.message}`);
+  });
+}
+
+async function initRedisSubscribe() {
+  console.log("Subscribed to logs....");
+  subscriber.psubscribe("logs:*");
+  subscriber.on("pmessage", (pattern, channel, message) => {
+    io.to(channel).emit("message", message);
+  });
+}
+
+// initRedisSubscribe();
+
+app.listen(PORT, () => console.log(`API Server Running..${PORT}`));
---
+++ build-server/script.js
@@ -0,0 +1,75 @@
+const { exec } = require("child_process");
+const path = require("path");
+const fs = require("fs");
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+const mime = require("mime-types");
+// const Redis = require("ioredis");
+require("dotenv").config();
+
+// const publisher = new Redis("");
+
+const s3Client = new S3Client({
+  region: "us-west-1",
+  credentials: {
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  },
+});
+
+const PROJECT_ID = process.env.PROJECT_ID; //import value from inside docker container's .env
+
+init(PROJECT_ID);
+
+// function publishLog(log) {
+//   publisher.publish(`logs:${PROJECT_ID}`, JSON.stringify({ log }));
+// }
+
+async function init(projectId) {
+  console.log("Executing script.js");
+  console.log(`SLUG_NAME from api server: ${projectId}`);
+  //   publishLog("Build Started...");
+  const outDirPath = path.join(__dirname, "output");
+
+  const p = exec(`cd ${outDirPath} && npm install && npm run build`);
+
+  p.stdout.on("data", function (data) {
+    console.log(data.toString());
+    // publishLog(data.toString());
+  });
+
+  p.stderr.on("error", function (data) {
+    console.log("Error", data.toString());
+    // publishLog(`error: ${data.toString()}`);
+  });
+
+  p.on("close", async function () {
+    console.log("Build Complete");
+    // publishLog(`Build Complete`);
+    const distFolderPath = path.join(__dirname, "output", "dist");
+    const distFolderContents = fs.readdirSync(distFolderPath, {
+      recursive: true,
+    });
+
+    // publishLog(`Starting to upload`);
+    for (const file of distFolderContents) {
+      const filePath = path.join(distFolderPath, file);
+      if (fs.lstatSync(filePath).isDirectory()) continue;
+
+      console.log("uploading", filePath);
+      //   publishLog(`uploading ${file}`);
+
+      const command = new PutObjectCommand({
+        Bucket: "vercel-git-builder-files",
+        Key: `__outputs/${PROJECT_ID}/${file}`,
+        Body: fs.createReadStream(filePath),
+        ContentType: mime.lookup(filePath),
+      });
+
+      await s3Client.send(command);
+      //   publishLog(`uploaded ${file}`);
+      console.log("uploaded", filePath);
+    }
+    // publishLog(`Done`);
+    console.log("Done...");
+  });
+}
